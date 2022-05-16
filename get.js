3a02fe7bdea36b20d31231bf52419c1de68a0c07
Smitty <me@smitop.com>
2022-05-16 13:12:11 -0400

    better

--- get.js
+++ get.js
@@ -1,7 +1,17 @@
 const commits = require("./commits.json");
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 const { execSync } = require("child_process");
 
 execSync("mkdir standalone");
 execSync("cd standalone");
 
+const commitUrl = c => `https://sourcegraph.com/github.com/uup-dump/standalone@${c}/-/raw?format=zip`;
+
+function doCommit(commit) {
+    execSync(`cd standalone && rm -rf ~/remake-standalone/standalone/* && rm -rf .github && wget -O raw.zip "${commit.hash}"`);
+    execSync("cd standalone && unzip raw -d . && rm raw.zip");
+    execSync(`cd standalone && git add . && git commit -m "${commit.msg}" --author="${commit.name} <${commit.email}>" --date="${commit.date}"`)
+}
+
+doCommit(commits[20])
+// for (let i = commits.length; i > 0; i--) doCommit(commits[i]);
