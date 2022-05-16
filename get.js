6d2d31e2f6c0eb6dbfd7058332f2025129b6b2f3
Smitty <me@smitop.com>
2022-05-16 13:24:30 -0400

    make work

--- get.js
+++ get.js
@@ -2,16 +2,20 @@ const commits = require("./commits.json");
 // const fetch = require("node-fetch");
 const { execSync } = require("child_process");
 
-execSync("mkdir standalone");
-execSync("cd standalone");
+execSync("rm -rf /tmp/standalone");
+execSync("mkdir /tmp/standalone && cd /tmp/standalone && git init");
 
 const commitUrl = c => `https://sourcegraph.com/github.com/uup-dump/standalone@${c}/-/raw?format=zip`;
-
-function doCommit(commit) {
-    execSync(`cd standalone && rm -rf ~/remake-standalone/standalone/* && rm -rf .github && wget -O raw.zip "${commit.hash}"`);
-    execSync("cd standalone && unzip raw -d . && rm raw.zip");
-    execSync(`cd standalone && git add . && git commit -m "${commit.msg}" --author="${commit.name} <${commit.email}>" --date="${commit.date}"`)
+function escapeShell(arg) {
+    return arg.replace(/'/g, `'\\''`).replace(/`/g, "\\`");
 }
 
-doCommit(commits[20])
-// for (let i = commits.length; i > 0; i--) doCommit(commits[i]);
+
+function doCommit(commit) {
+    execSync(`cd /tmp/standalone && rm -rf /tmp/standalone/* && rm -rf .github .gitmodules && wget -O raw.zip '${escapeShell(commitUrl(commit.hash))}'`);
+    execSync("cd /tmp/standalone && unzip raw -d . && rm raw.zip");
+    execSync(`GIT_COMITTER_NAME="${escapeShell(commit.name)}" GIT_COMITTER_EMAIL="${escapeShell(commit.email)}" cd /tmp/standalone && git add . && git commit --allow-empty -m "${escapeShell(commit.msg)}" --author="${escapeShell(commit.name)} <${escapeShell(commit.email)}>" --date="${escapeShell(commit.date)}"`)
+}
+
+// doCommit(commits[20])
+for (let i = commits.length - 1; i >= 0; i--) doCommit(commits[i]);
