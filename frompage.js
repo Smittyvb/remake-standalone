99f192add356773b2aabb3f94cdbeb284a8ab320
Smitty <me@smitop.com>
2022-05-16 14:47:18 -0400

    use whole commit desc

--- frompage.js
+++ frompage.js
@@ -1,8 +1,10 @@
+// show all on https://sourcegraph.com/github.com/uup-dump/standalone/-/commits
+[...document.querySelectorAll(".GitCommitNode-module__message-toggle_1N76-")].map(b => b.click());
 console.log(JSON.stringify([...document.querySelectorAll(".GitCommitNode-module__byline_2cHFd")].map(x => ({
     msg: x.querySelector(".AnchorLink-module__anchor-link_2e_jy").textContent,
     email: x.querySelector("div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2)").dataset.tooltip,
     name: x.querySelector("div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2)").textContent,
     date: x.querySelector("div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(3)").dataset.tooltip,
-    hash: x.querySelector(".AnchorLink-module__anchor-link_2e_jy").href.split("/")[8]
+    hash: x.querySelector(".AnchorLink-module__anchor-link_2e_jy").href.split("/")[8],
+    msgExtra: x.querySelector(".GitCommitNode-module__message-body_3sp3D")?.textContent
 }))))
-
--- get.js
+++ get.js
@@ -14,7 +14,9 @@ function escapeShell(arg) {
 function doCommit(commit) {
     execSync(`cd /tmp/standalone && rm -rf /tmp/standalone/* && rm -rf .github .gitmodules && wget -O raw.zip '${escapeShell(commitUrl(commit.hash))}'`);
     execSync("cd /tmp/standalone && unzip raw -d . && rm raw.zip");
-    execSync(`GIT_COMITTER_NAME="${escapeShell(commit.name)}" GIT_COMITTER_EMAIL="${escapeShell(commit.email)}" cd /tmp/standalone && git add . && git commit --allow-empty -m "${escapeShell(commit.msg)}" --author="${escapeShell(commit.name)} <${escapeShell(commit.email)}>" --date="${escapeShell(commit.date)}"`)
+    execSync(`cd /tmp/standalone && git config user.email '${escapeShell(commit.email)}'`);
+    execSync(`cd /tmp/standalone && git config user.name '${escapeShell(commit.name)}'`);
+    execSync(`export GIT_COMMITTER_DATE="${escapeShell(commit.date)}" && cd /tmp/standalone && git add . && git commit --allow-empty -m "${escapeShell(commit.msg)}" ${commit.msgExtra ? `-m '${escapeShell(commit.msgExtra)}'` : ""} --author="${escapeShell(commit.name)} <${escapeShell(commit.email)}>" --date="${escapeShell(commit.date)}"`)
 }
 
 // doCommit(commits[20])
